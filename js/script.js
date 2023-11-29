37d934c2edc49f7a1de6f1df00f96d63bde23110
Anymoose98 <daniel.digiacomo98@gmail.com>
2023-11-29 19:26:11 +0100

    esercizio base + bonus finito

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-function creazionequadrato(){
-    
+// Funzione Quadrato
+function creazionequadrato(num){
+    const quadrato = document.createElement("div");
+    quadrato.classList.add("quadrato");
+    quadrato.addEventListener("click",function(){
+        this.classList.toggle("bg-click")
+        console.log("casella n." + num)
+    })
+    return quadrato;
 }
+
+// Assegno il contenitore dei quadrati
+const contenitore_quadrati = document.getElementById("contenitore")
+
+// Bottone
+const bottone = document.querySelector("button")
+bottone.addEventListener("click",function(){
+    
+    // Ripulire il codice
+    contenitore_quadrati.innerHTML = "";
+
+    // Creo i quadrati
+    for(let i = 0; i < 100; i++){
+        let quadrato = creazionequadrato(i + 1);
+        contenitore_quadrati.appendChild(quadrato)
+    }
+})
